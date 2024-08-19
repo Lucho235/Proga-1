f7ca1453e7b9a204e5f2cd1b9fdfe96df8634be7
Lucho235 <llamas@uade.edu.ar>
2024-08-19 16:02:45 -0300

    MOdificacion

--- desafio semana 1.py
+++ desafio semana 1.py
@@ -6,35 +6,35 @@ def mostrar_matriz(matriz_calificaciones, n, m):
     for j in range(1, m+ 1):
         print(" "*4, "Materia", j, end="")
     print()
-# Encabezados de ESTUDIANTES
+    # Encabezados de ESTUDIANTES
     i = 0
     for fila in (matriz_calificaciones):
         print("Estudiante: ", (i + 1), end="")
-    for calificacion in fila:
-        print(" "*10, calificacion, end=" ")
-    print()
-    i = i + 1
+        for calificacion in fila:
+            print(" "*10, calificacion, end=" ")
+        print()
+        i = i + 1
 
 def promedio_calificaciones(matriz_calificaciones):
     i = 0
     for fila in matriz_calificaciones:
         suma = 0
         j = 0
-    while j < len(fila):
-        suma += fila[j]
-        j = j + 1
-    promedio = suma / len(fila)
-    print("El promedio de calificaciones del Estudiante: ", (i + 1), " es: ", (promedio))
-    i += 1
+        while j < len(fila):
+            suma += fila[j]
+            j = j + 1
+        promedio = suma / len(fila)
+        print("El promedio de calificaciones del Estudiante: ", (i + 1), " es: ", (promedio))
+        i += 1
 
 def calcular_promedio_materias(matriz_calificaciones):
     num_materias = len(matriz_calificaciones[0])
     for j in range(num_materias):
         suma = 0
-    for fila in matriz_calificaciones:
-        suma = suma + fila[j]
-    promedio = suma / len(matriz_calificaciones)
-    print("El promedio de calificaciones en la Materia", (j + 1), " es:", (promedio))
+        for fila in matriz_calificaciones:
+            suma = suma + fila[j]
+        promedio = suma / len(matriz_calificaciones)
+        print("El promedio de calificaciones en la Materia", (j + 1), " es:", (promedio))
 
 # Definir el número de estudiantes y materias
 n = int(input("Ingrese el nro de estudiantes: ")) # seria nuestra cantidad de filas
